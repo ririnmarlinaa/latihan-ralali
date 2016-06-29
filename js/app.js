3c38707fcfd5ba2d6f2739b1af8465e234b2b04f
ririnmarlinaa <ririn.marlina@ralali.com>
2016-06-29 12:04:10 +0700

    test

--- js/app.js
+++ js/app.js
@@ -13,37 +13,69 @@ app.config(function($locationProvider,$routeProvider){
     $locationProvider.html5Mode(true);
 });
 
-app.controller("myCtrl", function($scope,$routeParams){
+app.controller("myCtrl", function($scope,$routeParams,$location){
 
 $scope.articles=[{
-       title:"Title number 1",
-       description:"Lorem ipsum dolor sit amet. Integer volutpat justo quis leo interdum tempus. Duis ut urna ante. Ut mauris tortor, pellentesque vitae finibus nec, accumsan vel mi. Mauris sagittis sapien mi, non mattis libero porttitor sit amet. Curabitur congue rutrum risus quis convallis. Proin pulvinar ex eu tellus porta efficitur. Phasellus lobortis vestibulum interdum. Duis pellentesque ex suscipit ligula hendrerit, in vulputate tortor porttitor. In nec turpis dapibus, feugiat orci eget, ornare nisl. Nullam vehicula magna sit amet nisl efficitur, sit amet condimentum turpis convallis. Donec tempor ultrices tellus vel placerat. Nunc vehicula tristique massa a tincidunt. Nulla ac justo at arcu vulputate semper vitae nec lacus.",
-       author:"Galih",
-       vote:"10",
-       answer:"0",
-       id:"1"
-   },
-   {
-       title:"Title number 2",
-       description:"Lorem ipsum dolor sit amet. Integer volutpat justo quis leo interdum tempus. Duis ut urna ante. Ut mauris tortor, pellentesque vitae finibus nec, accumsan vel mi. Mauris sagittis sapien mi, non mattis libero porttitor sit amet. Curabitur congue rutrum risus quis convallis. Proin pulvinar ex eu tellus porta efficitur. Phasellus lobortis vestibulum interdum. Duis pellentesque ex suscipit ligula hendrerit, in vulputate tortor porttitor. In nec turpis dapibus, feugiat orci eget, ornare nisl. Nullam vehicula magna sit amet nisl efficitur, sit amet condimentum turpis convallis. Donec tempor ultrices tellus vel placerat. Nunc vehicula tristique massa a tincidunt. Nulla ac justo at arcu vulputate semper vitae nec lacus.",
-       author:"Ririn",
-       vote:"11",
-       answer:"0",
-       id:"2"
-   },
-   {
-       title:"Title number 3",
-       description:"Lorem ipsum dolor sit amet. Integer volutpat justo quis leo interdum tempus. Duis ut urna ante. Ut mauris tortor, pellentesque vitae finibus nec, accumsan vel mi. Mauris sagittis sapien mi, non mattis libero porttitor sit amet. Curabitur congue rutrum risus quis convallis. Proin pulvinar ex eu tellus porta efficitur. Phasellus lobortis vestibulum interdum. Duis pellentesque ex suscipit ligula hendrerit, in vulputate tortor porttitor. In nec turpis dapibus, feugiat orci eget, ornare nisl. Nullam vehicula magna sit amet nisl efficitur, sit amet condimentum turpis convallis. Donec tempor ultrices tellus vel placerat. Nunc vehicula tristique massa a tincidunt. Nulla ac justo at arcu vulputate semper vitae nec lacus.",
-       author:"Bowo",
-       vote:"22",
-       answer:"0",
-       id:"3"
-    }];
+      title:"Title number 1",
+      id:"1",
+      description:"1 Lorem ipsum dolor sit amet. Integer volutpat justo quis leo interdum tempus. Duis ut urna ante. Ut mauris tortor, pellentesque vitae finibus nec, accumsan vel mi. Mauris sagittis sapien mi, non mattis libero porttitor sit amet. Curabitur congue rutrum risus quis convallis. Proin pulvinar ex eu tellus porta efficitur. Phasellus lobortis vestibulum interdum. Duis pellentesque ex suscipit ligula hendrerit, in vulputate tortor porttitor. In nec turpis dapibus, feugiat orci eget, ornare nisl. Nullam vehicula magna sit amet nisl efficitur, sit amet condimentum turpis convallis. Donec tempor ultrices tellus vel placerat. Nunc vehicula tristique massa a tincidunt. Nulla ac justo at arcu vulputate semper vitae nec lacus.",
+      author:"Galih",
+      vote:"10",
+      answer:"0",
+      answers:[]
+  },
+  {
+      title:"Title number 2",
+      id:"2",
+      description:"2 Lorem ipsum dolor sit amet. Integer volutpat justo quis leo interdum tempus. Duis ut urna ante. Ut mauris tortor, pellentesque vitae finibus nec, accumsan vel mi. Mauris sagittis sapien mi, non mattis libero porttitor sit amet. Curabitur congue rutrum risus quis convallis. Proin pulvinar ex eu tellus porta efficitur. Phasellus lobortis vestibulum interdum. Duis pellentesque ex suscipit ligula hendrerit, in vulputate tortor porttitor. In nec turpis dapibus, feugiat orci eget, ornare nisl. Nullam vehicula magna sit amet nisl efficitur, sit amet condimentum turpis convallis. Donec tempor ultrices tellus vel placerat. Nunc vehicula tristique massa a tincidunt. Nulla ac justo at arcu vulputate semper vitae nec lacus.",
+      author:"Ririn",
+      vote:"11",
+      answer:"2",
+      answers:[
+        {
+           author : "Agung",
+           content : "dudududuudududududu",
+           vote :"11"
 
-$scope.init=function(){
+        },{
+           author : "Meise",
+           content : "lalallalala",
+           vote :"-1"
+        },{
+           author : "bagus",
+           content : "Donec tempor ultrices tellus vel placerat. Nunc vehicula tristique massa a tincidunt. Nulla ac justo at arcu vulputate",
+           vote :"-1"
+        }
 
-console.init($routeParams.id);
+      ]
+  },
+  {
+      title:"Title number 3",
+      id:"3",
+      description:"3 Lorem ipsum dolor sit amet. Integer volutpat justo quis leo interdum tempus. Duis ut urna ante. Ut mauris tortor, pellentesque vitae finibus nec, accumsan vel mi. Mauris sagittis sapien mi, non mattis libero porttitor sit amet. Curabitur congue rutrum risus quis convallis. Proin pulvinar ex eu tellus porta efficitur. Phasellus lobortis vestibulum interdum. Duis pellentesque ex suscipit ligula hendrerit, in vulputate tortor porttitor. In nec turpis dapibus, feugiat orci eget, ornare nisl. Nullam vehicula magna sit amet nisl efficitur, sit amet condimentum turpis convallis. Donec tempor ultrices tellus vel placerat. Nunc vehicula tristique massa a tincidunt. Nulla ac justo at arcu vulputate semper vitae nec lacus.",
+      author:"Bowo",
+      vote:"22",
+      answer:"1",
+      answers:[
+        {
+           author : "Ashari",
+           content : "dedeeddededededede",
+           vote :"-5"
+        }
+      ]
+  }];
 
-}
 
-});
+
+    $scope.init=function(){
+        console.log($routeParams.id);
+
+        for (i = 0; i < $scope.articles.length; i++){
+            if($scope.articles[i].id == $routeParams.id){
+                $scope.idx = $scope.articles[i];
+                break;
+            }
+        }
+    }
+
+    });
